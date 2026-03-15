f0215b4320255d777789b60732ce738277740712
Link4Electronics <link4electronics@gmail.com>
2026-03-15 13:11:29 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,6 +7,7 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
+    glib2          \
     gtk3           \
     jre-openjdk    \
     libdecor       \
